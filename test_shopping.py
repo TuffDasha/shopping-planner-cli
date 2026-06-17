fa3b027550097f6c8526aa1d4b0197d4f3d6960c
TuffDasha <dashapoopii6969@gmail.com>
2026-06-17 11:44:53 +0300

    Fix: виправлено логіку ізоляції автотесту

--- test_shopping.py
+++ test_shopping.py
@@ -1,12 +1,14 @@
 import unittest
-from main import get_next_id
+# Тимчасово підміняємо файл даних для тесту, щоб не чіпати твій основний json
+import main
+main.DATA_FILE = "shopping_list_test.json"
 
 class TestShoppingPlanner(unittest.TestCase):
     
     def test_get_next_id_empty_list(self):
         """Тест: якщо список порожній, перший ID має бути 1"""
         empty_list = []
-        result = get_next_id(empty_list)
+        result = main.get_next_id(empty_list)
         self.assertEqual(result, 1)
 
     def test_get_next_id_with_items(self):
@@ -15,8 +17,8 @@ class TestShoppingPlanner(unittest.TestCase):
             {"id": 1, "name": "Молоко", "price": 40.0, "quantity": 1, "category": "Продукти", "is_bought": False},
             {"id": 5, "name": "Хліб", "price": 20.0, "quantity": 1, "category": "Продукти", "is_bought": True}
         ]
-        result = get_next_id(sample_data)
-        # Максимальний ID зараз 5, отже наступний має бути 6
+        result = main.get_next_id(sample_data)
+        # Максимальний ID у списку — 5, отже наступний має бути 6
         self.assertEqual(result, 6)
 
 if __name__ == "__main__":
